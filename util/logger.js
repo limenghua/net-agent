d42b5b79e04884bbd2a3a9f05b39206375ff3c0c
limenghua <menghua.li@netbraintech.com>
2017-09-05 18:34:40 +0800

    log the package count

--- util/logger.js
+++ util/logger.js
@@ -8,8 +8,24 @@ log4js.configure({
 var logger = log4js.getLogger();
 logger.level = 'debug';
 
-logger.headerlog =  function(header,name){
-    logger.debug('header of type:%d,identity:%d,module:%s',header.type,header.identity,name);
-}
+logger.headerlog =  function(){
+    let totalPackages = 0;
+    let lastSecondsPackages = 0;
+    let packagePerSecond = 0;
+
+    setInterval(()=>{
+        packagePerSecond = totalPackages - lastSecondsPackages;
+        lastSecondsPackages = totalPackages;
+    },1000);
+
+    return function(header,name){
+        if(header.type ===1)totalPackages ++;
+
+        logger.debug('header of type:%d,identity:%d,module:%s,totalPackage:%d, %d packages/s'
+            ,header.type,header.identity,name,totalPackages,packagePerSecond);
+    }
+}();
+
+
 
 module.exports = logger;
