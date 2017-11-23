d235dd634cea05ab58be2f2cad94e7e7ee24b78a
limenghua <menghua.li@netbraintech.com>
2017-11-23 18:18:14 +0800

    change the script path

--- demo/start.bat
+++ demo/start.bat
@@ -1,9 +1,9 @@
-start node echo-service-demo.js 9000
-start node client-agent-demo.js --local-port 5000 --remote-port 6000
+start node echo-service.js 9000
+start node ../script/client-agent.js --local-port 5000 --remote-port 6000
 
 ping 127.0.0.1 -n 4
 
-start node service-agent-demo.js --service-port 9000 --service-host 127.0.0.1 --remote-port 6000 --remote-host 127.0.0.1
+start node ../script/service-agent.js --service-port 9000 --service-host 127.0.0.1 --remote-port 6000 --remote-host 127.0.0.1
 
 ping 127.0.0.1 -n 4
 
